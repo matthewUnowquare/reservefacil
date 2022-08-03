a0a1fc09064b955c6f00dbae20af91c40d1d29dd
camilo kosniowski <camilo.kosniowski@unosquare.com>
2022-08-03 16:41:30 -0500

    addin i18n dependence

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,20 @@
-import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
-import { Login } from "./components/auth/Login";
-import Home from "./components/Home/Home";
-import { Layout } from "./components/Layout/Layout";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './components/auth/Login';
+import Home from './components/Home/Home';
+import { Layout } from './components/Layout/Layout';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/languaje' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Layout>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import i18n (needs to be bundled ;))
+import './components/Lang/i18n';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
